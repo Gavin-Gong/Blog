172296fee3de83b384861acd6392b46fd80e4698
KefenGong <kefengong@outlook.com>
2016-11-17 09:35:56 +0800

    comment model Schema

---
+++ models/comment.js
@@ -0,0 +1,16 @@
+// TODO 插件
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var commentSchema = new Schema({
+  content: String,
+  username: String,
+  avatar: String,
+  is_read: Boolean,
+  posts: String,
+});
+commentSchema.method('fetchComment', function () {
+  console.log(this);
+});
+var commentModel = mongoose.model('Comment', commentSchema);
