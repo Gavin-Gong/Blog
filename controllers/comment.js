e6a513c243c2676b161610d371e1f24e8c38dc01
KefenGong <kefengong@outlook.com>
2016-11-25 22:17:15 +0800

    add comment part

---
+++ controllers/comment.js
@@ -0,0 +1,12 @@
+let commentModel = require('../models/comment');
+
+exports.addComment = (req, res) => {
+  console.log(req.body);
+  console.log(req.params.post_id);
+  console.log(req.session.user);
+  if (req.body.comment.trim() && req.session.user._id && req.params.post_id) {
+    commentModel.addComment(req.body.comment, req.session.user._id, req.params.post_id);
+  } else {
+    console.log('参数不全');
+  }
+};
--- models/comment.js
+++ models/comment.js
@@ -1,14 +1,46 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+const { timePlugin } = require('../lib/mongoPlugin');
 
 var commentSchema = new Schema({
   content: String,
   username: String,
   avatar: String,
   is_read: Boolean,
+  // TODO 增加留言审核
+  is_allow: {
+    type: Boolean,
+    default: false
+  },
   posts: String,
+  comment_from: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  comment_to: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'posts'
+  }
 });
 commentSchema.method('fetchComment', function () {
   console.log(this);
 });
-var commentModel = mongoose.model('Comment', commentSchema);
+
+commentSchema.plugin(timePlugin);
+var commentModel = mongoose.model('comments', commentSchema);
+
+// module.exports =commentModel;
+
+exports.getCommentsByUserId = (user_id) => {
+  return commentModel.find({comment_to: user_id}).populate('comment_from');
+};
+exports.getCommentsByPostId = (post_id) => {
+  return commentModel.find({comment_from: post_id}).populate('comment_to');
+};
+exports.addComment = (content, user_id, post_id) => {
+  return commentModel.create({
+    content,
+    comment_from: user_id,
+    comment_to: post_id
+  });
+};
